fb6238e741c7e264eba117b27fa911c25c76c527
Hsiaoming Yang <me@lepture.com>
2018-11-06 17:03:05 +0900

    Version bump 0.3

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -6,10 +6,10 @@
 
     A captcha library that generates audio and image CAPTCHAs.
 
-    :copyright: (c) 2015 - 2017 by Hsiaoming Yang.
+    :copyright: (c) 2015 - 2018 by Hsiaoming Yang.
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = '0.2.4'
+__version__ = '0.3'
 __author__ = 'Hsiaoming Yang <me@lepture.com>'
 __homepage__ = 'https://github.com/lepture/captcha'
