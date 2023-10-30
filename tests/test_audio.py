cb528e7a78f912d697b137fd43d2b1f48a7b29a7
Ari Archer <ari.web.xyz@gmail.com>
2023-10-30 19:30:23 +0200

    change 22050 hz 16 bit wavs to 8000 hz 8 bit ones
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -5,7 +5,7 @@ from captcha.audio import AudioCaptcha
 
 def test_audio_generate():
     captcha = AudioCaptcha()
-    data = captcha.generate('1234')
+    data = captcha.generate('a123')
     assert isinstance(data, bytearray)
     assert bytearray(b'RIFF') in data
 
